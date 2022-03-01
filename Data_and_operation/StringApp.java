41b4a231e5d856f27cce485bf7ce37415c5c239c
parktae <parktae004@gmail.com>
2022-03-01 19:46:19 +0900

    문자열 다루기 까지 commit

---
+++ Data_and_operation/StringApp.java
@@ -0,0 +1,20 @@
+
+public class StringApp {
+	public static void main(String[] args) {
+		
+		// Character VS String
+		System.out.println("Hello World"); // String
+		System.out.println('H'); // Character
+		System.out.println("H"); // String
+		
+		System.out.println("Hello "
+				+ "World");
+		
+		// new line
+		System.out.println("Hello \n World");
+		
+		// escape
+		System.out.println("Hello \"World\"");
+		
+	}
+}
---
+++ Data_and_operation/StringOperation.java
@@ -0,0 +1,14 @@
+
+public class StringOperation {
+	public static void main(String[] args) {
+		
+		// 문자가 몇글자인지 알려줌
+		System.out.println("Hello World".length()); // 11
+		
+		// 문자열 바꾸기
+		System.out.println("Hello, [[[name]]] ... bye.".replace("[[[name]]]", "duru"));
+		
+		
+		
+	}
+}
