c94012417af17998439a7ea10c47530c8fbfe651
parktae <parktae004@gmail.com>
2022-03-01 19:55:25 +0900

    변수에 대해 공부

---
+++ Data_and_operation/Variable.java
@@ -0,0 +1,17 @@
+
+public class Variable {
+	public static void main(String[] args) {
+		
+		// 어떤 데이터 타입이 들어가는지 정해주기
+		int a = 1; // Number -> integer(정수)
+		System.out.println(a);
+		
+		
+		double b = 1.1; // real number -> double ... -2.0, -1.0, 0 1.0, 2.0 ....
+		System.out.println(b);
+		
+		
+		String c = "Hello World"; // 문자열 
+		System.out.println(c);
+	}
+}
